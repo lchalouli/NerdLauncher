f98a0d778426cb9428f9c75254c14d41498b6722
Chalouli <Lela>
2017-09-08 17:07:31 +0200

    add application icons

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -57,20 +60,34 @@ public class NerdLauncherFragment extends Fragment {
         Log.i(TAG, "Found " + activities.size() + " activities");
     }
 
-    private class ActivityHolder extends RecyclerView.ViewHolder {
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
+        private ImageView mIconImageView;
 
         public ActivityHolder(View itemView) {
             super(itemView);
-            mNameTextView = (TextView) itemView;
+            mNameTextView = itemView.findViewById(R.id.activity_text_view);
+            mIconImageView = itemView.findViewById(R.id.icon_image_view);
         }
 
         public void bindActivity(ResolveInfo resolveInfo) {
             mResolveInfo = resolveInfo;
             PackageManager packageManager = getActivity().getPackageManager();
             CharSequence label = mResolveInfo.loadLabel(packageManager);
+            Drawable icon = mResolveInfo.loadIcon(packageManager);
             mNameTextView.setText(label);
+            mNameTextView.setOnClickListener(this);
+            mIconImageView.setImageDrawable(icon);
+        }
+
+        @Override
+        public void onClick(View view) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+            Intent intent = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName, activityInfo.name)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 
@@ -84,7 +101,7 @@ public class NerdLauncherFragment extends Fragment {
 
         @Override
         public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(getActivity()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.icon_list_item, parent, false);
             return new ActivityHolder(view);
         }
 
